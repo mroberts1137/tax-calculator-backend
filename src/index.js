68de431e39dc5bef2ff792c02c5b652eb271b6a2
mroberts1137 <mroberts1137@gmail.com>
2026-05-08 20:23:02 -0400

    un-comment-out mongoose config params

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/taxcalc', {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .then(() => {
     console.log('Connected to MongoDB');
