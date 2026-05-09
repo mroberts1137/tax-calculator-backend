c4ab2595eea6ee9922e7ffeeb86fa0d83c6cfc5c
mroberts1137 <mroberts1137@gmail.com>
2026-05-08 20:22:03 -0400

    fix temporary hardcoded port

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use('/api/tax', taxRoutes);
 const { errorHandler } = require('./middleware/errorHandler');
 app.use(errorHandler);
 
-const PORT = 5000; //process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/taxcalc', {
